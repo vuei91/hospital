8925b50ca50ab1dc708a11658b8df19d712efea1
vuei91 <wkwk2805u@naver.com>
2024-05-03 16:32:37 +0900

    linux에서 해결진행

--- app/_constants/index.js
+++ app/_constants/index.js
@@ -1,2 +1,3 @@
-export const BACKEND_URL = "http://localhost:8080";
+export const BACKEND_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080";
 export const MAP_CLIENT_ID = "v78r0xpip1";
