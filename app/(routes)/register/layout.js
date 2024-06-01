a52f1d07b3d84fccdab81058c3532fdd3acca115
vuei91 <wkwk2805u@naver.com>
2024-06-01 17:33:54 +0900

    useSearchParams 문제해결

--- app/(routes)/register/layout.js
+++ app/(routes)/register/layout.js
@@ -1,97 +1,14 @@
 "use client";
-import React from "react";
-import TopNav from "@/app/_components/TopNav";
-import BottomButton from "@/app/_components/BottomButton";
-import patientInfoStore from "@/app/_service/patientInfoStore";
-import {
-  useCreatePatientMutation,
-  useUpdatePatientMutation,
-} from "@/app/_hooks/usePatientMutation";
-import { Modal } from "antd-mobile";
-import { useRouter, useSearchParams } from "next/navigation";
-import { usePatientOneQuery } from "@/app/_hooks/usePatientQuery";
+import React, { Suspense } from "react";
+import RegisterLayout from "@/app/(routes)/register/_components/RegisterLayout";
+import Loading from "@/app/(routes)/register/loading";
 
-const Layout = ({ children }) => {
-  const searchParams = useSearchParams();
-  const id = searchParams.get("id");
-  const router = useRouter();
-  const { name, phone, grade, address, clear } = patientInfoStore(
-    (state) => state,
-  );
-  const { createPatient } = useCreatePatientMutation();
-  const { updatePatient } = useUpdatePatientMutation(id);
-  const { refetch } = usePatientOneQuery(id);
-  const create = async (latitude, longitude) => {
-    await createPatient(
-      { name, phone, grade, address, latitude, longitude },
-      { onSuccess },
-    );
-  };
-  const update = async (latitude, longitude) => {
-    await updatePatient(
-      { name, phone, grade, address, latitude, longitude },
-      { onSuccess },
-    );
-  };
-  const onSuccess = (data) => {
-    if (data?.status === "success") {
-      refetch().then(() => {
-        router.push("/enroll/choice-patient");
-        clear();
-      });
-    } else {
-      Modal.alert({
-        content: "문제가 있으니 다시 입력해주세요",
-        confirmText: "확인",
-      });
-    }
-  };
-  const submit = async () => {
-    if (!name || !name.replace(/ /g, "")) {
-      return Modal.alert({
-        content: "이름을 입력해주세요",
-        confirmText: "확인",
-      });
-    }
-    if (!phone || !phone.replace(/ /g, "")) {
-      return Modal.alert({
-        content: "휴대폰번호를 입력해주세요",
-        confirmText: "확인",
-      });
-    }
-    if (!grade) {
-      return Modal.alert({
-        content: "요양등급을 입력해주세요",
-        confirmText: "확인",
-      });
-    }
-    if (!address || !address.replace(/ /g, "")) {
-      return Modal.alert({
-        content: "대상자의 주소를 입력해주세요",
-        confirmText: "확인",
-      });
-    }
-    const geocoder = new window.kakao?.maps?.services?.Geocoder();
-    // 주소로 좌표를 검색합니다
-    geocoder?.addressSearch(address, async function (result, status) {
-      let longitude, latitude;
-      // 정상적으로 검색이 완료됐으면
-      if (status === kakao.maps.services.Status.OK) {
-        longitude = result[0].x;
-        latitude = result[0].y;
-      }
-      !id
-        ? await create(latitude, longitude)
-        : await update(latitude, longitude);
-    });
-  };
+const RegisterRootLayout = ({ children }) => {
   return (
-    <>
-      <TopNav title={"대상자 정보입력"} isBack />
-      <div className="container">{children}</div>
-      <BottomButton text={!id ? "등록" : "수정"} onClick={submit} />
-    </>
+    <Suspense fallback={<Loading />}>
+      <RegisterLayout>{children}</RegisterLayout>
+    </Suspense>
   );
 };
 
-export default Layout;
+export default RegisterRootLayout;
